f84c6ca2a2df4e591cb7da9c3fd7f0b413c03d0c
yossy4411 <yossy4411.dev@gmail.com>
2024-11-19 16:25:19 +0900

    最初の設定画面を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod japanese;
+mod english;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Select language: ");
+    println!("1. English");
+    println!("2. 日本語");
+
+    let mut language = String::new();
+    std::io::stdin().read_line(&mut language).unwrap();
+    let language = language.trim();
+
+    match language {
+        "1" => english::menu(),
+        "2" => japanese::menu(),
+        _ => println!("Invalid selection"),
+    }
 }
